22e99e0a32bfbaffee46f6180b6133ade7098776
khanhhung0810 <lhkhungg@gmail.com>
2023-09-13 09:45:38 +0700

    add1

--- ActivityLifeCycle/app/src/main/java/com/khanhhung/activitylifecycle/MainActivity.java
+++ ActivityLifeCycle/app/src/main/java/com/khanhhung/activitylifecycle/MainActivity.java
@@ -3,12 +3,24 @@ package com.khanhhung.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    String tag = "Lifecycle Step";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(tag, "In the onCreate() event");
     }
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(tag, "In the onStart() event");
+        Log.d(tag, "Khởi tạo biến ở đây");
+
+    }
+
+
 }
